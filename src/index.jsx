764ded57728b57a274eb292ff202dcb3116fbf1a
sivarao1246-pixel <sivarao1246@gmail.com>
2025-09-03 11:16:58 +0530

    Fix CRA entry point and rebuild

--- src/index.jsx
+++ src/index.jsx
@@ -5,6 +5,10 @@ import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
 import './index.css'
 
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
